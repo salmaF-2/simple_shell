0022b5f414973a5d4cbce387dd46a62192b253a6
salma alfadili <salmafadili003@gmail.com>
2023-05-18 07:07:22 -0700

    remove old files

--- header.h
+++ header.h
@@ -10,18 +10,6 @@
 #include <sys/types.h>
 #include <stddef.h>
 
-
-/**
- * struct built_d - def the built-in functions.
- * @b: The name in  cmd
- * p: pointer to built-in function that we want
- */
-typedef struct built_d
-{
-	char *b;
-	void (*p)(char *);
-} built_t;
-
 /*--------function used for task1--------*/
 void execute_prg(char **cmd, int *j, char **av);
 void _execute(char *cmd, char *av);
@@ -29,13 +17,7 @@ void _strtok(char *string, char *separator, char **arg);
 /*-------------------------------------------------------*/
 
 
-extern char **environment;
+extern char **environ;
 /*-----------------*/
-void (*_built_in(int num, ...))(char *func);
-int check_builtin(char **cmd, char *input);
-void b_exit(char *input);
-void b_cd(char *arguments);
-void b_env(char *string);
-void free_dptr(char **to_be_freed);
-char **tokenize(char *str, char *delimiter);
+void b_exit(void);
 #endif

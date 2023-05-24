e0c8b18c611886759830fe2c7bf68cf0d5480c8d
n.bakenchich@gmail.com <n.bakenchich@gmail.com>
2023-05-24 14:21:35 -0700

    test task1

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define MAX_INPUT_LENGTH 1024
+#define MAX_ARGS 64
+void displayPrompt(void);
+char **splitInput(char *input);
+int executeCommand(char **args);
+void freeMemory(char **args);
+#endif

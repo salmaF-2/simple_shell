a4dcbe9fff9a373a28de7b2a2beeee724adbc5eb
n.bakenchich@gmail.com <n.bakenchich@gmail.com>
2023-05-25 11:49:54 -0700

    test output

--- header.h
+++
@@ -1,6 +0,0 @@
-/* function for buildins*/
-void _cd(char *cmd);
-void _env(attribute((unused))char *cmd);
-void _exit(char *cmd);
-void (find_buildin_s(charstr))(char *str);
-int _buildins2(char *line, charcmd);
